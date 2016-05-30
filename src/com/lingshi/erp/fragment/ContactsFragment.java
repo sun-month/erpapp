4bf19c5a2c51ab1888a2c05c994a96181e291838
sun-month <itchenhan1993@gmail.com>
2016-05-30 17:46:37 +0800

    开始编写联系人界面

--- src/com/lingshi/erp/fragment/ContactsFragment.java
+++ src/com/lingshi/erp/fragment/ContactsFragment.java
@@ -6,14 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
 import com.lingshi.erp.R;
 
 @SuppressLint("InflateParams")
 public class ContactsFragment extends Fragment {
+	private View view;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.contacts_fragment, null);
+		view = inflater.inflate(R.layout.contacts_fragment, null);
+		ExpandableListView contacts = (ExpandableListView) view
+				.findViewById(R.id.contacts_expand_list_view);
+		return view;
 	}
 }
